83162783f1d617d85b5a894252559aed780b764e
Alex Knott <alextknott@gmail.com>
2022-04-15 16:38:26 +0100

    Implement Routing for Homepage,Catalogpage and Cartpage with Navbar always rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./Components/Homepage/Homepage";
+import Catalogpage from "./Components/Catalog/Catalogpage";
+import Cartpage from "./Components/Cart/Cartpage";
+import Navbar from "./Components/Navbar/Navbar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="catalog" element={<Catalogpage />} />
+        <Route path="cart" element={<Cartpage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
