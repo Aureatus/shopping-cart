ac73b57f4d7955ade5d007721660b4603afeafe2
Alex Knott <alextknott@gmail.com>
2022-04-15 14:53:01 +0100

    Create initial react components and export them

---
+++ src/Components/Cart/Cartpage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cartpage = () => {
+  return <div></div>;
+};
+
+export default Cartpage;
---
+++ src/Components/Catalog/Catalogpage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Catalogpage = () => {
+  return <div>Catalog</div>;
+};
+
+export default Catalogpage;
