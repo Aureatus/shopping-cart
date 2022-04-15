023c9785b8774ddf678d4d2e6d6bfe5bd7e2cefb
Alex Knott <alextknott@gmail.com>
2022-04-15 19:57:37 +0100

    Add basic styling for index.jsx and import it

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+import "normalize.css";
+import "./Styles/index.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
