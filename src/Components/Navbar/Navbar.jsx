0a294909b806f46c4dc55ef0b353097a59d954ff
Alex Knott <alextknott@gmail.com>
2022-04-16 12:59:45 +0100

    Implement Links to urls for routing and a h1 title

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <header aria-label="Navigation Bar">Navbar</header>;
+  return (
+    <header aria-label="Navigation Bar">
+      <h1>Shopping Cart</h1>
+      <Link to="/">Home</Link>
+      <Link to="catalog">Catalog</Link>
+      <Link to="cart">Cart</Link>
+    </header>
+  );
 };
 
 export default Navbar;
