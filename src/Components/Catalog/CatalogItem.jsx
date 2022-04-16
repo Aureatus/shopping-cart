cffce2072964bb9faef9b384e63d44e1f56fc968
Alex Knott <alextknott@gmail.com>
2022-04-16 16:41:31 +0100

    Use props.e instead of props.element for p text

--- src/Components/Catalog/CatalogItem.jsx
+++ src/Components/Catalog/CatalogItem.jsx
@@ -6,7 +6,7 @@ const CatalogItem = (props) => {
   return (
     <div className="item" key={props.index}>
       <img src="" alt="" />
-      <p className="itemName">{props.element}</p>
+      <p className="itemName">{props.e}</p>
       <form>
         <input type="number" id="itemAmount" defaultValue={1} />
         <button type="submit" id="addToCart">
