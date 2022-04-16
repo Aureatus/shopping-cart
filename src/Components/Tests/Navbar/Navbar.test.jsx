93eb33e1ceb35ca97ca157050363297419957eb9
Alex Knott <alextknott@gmail.com>
2022-04-16 13:00:17 +0100

    Write tests to verify links are loading appropiate content

---
+++ src/Components/Tests/Navbar/Navbar.test.jsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import "@testing-library/jest-dom";
+
+import App from "../../../App";
+import userEvent from "@testing-library/user-event";
+
+test(`Full app navigating using Navbar Links`, async () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  userEvent.click(screen.getByRole("link", { name: /catalog/i }));
+  expect(
+    screen.getByRole("main", { name: /catalog section/i })
+  ).toBeInTheDocument();
+
+  userEvent.click(screen.getByRole("link", { name: /cart/i }));
+  expect(
+    screen.getByRole("main", { name: /cart section/i })
+  ).toBeInTheDocument();
+
+  userEvent.click(screen.getByRole("link", { name: /home/i }));
+  expect(
+    screen.getByRole("main", { name: /home section/i })
+  ).toBeInTheDocument();
+});
