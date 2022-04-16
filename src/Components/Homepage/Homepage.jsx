2f11cc40b3761b53a2375dd7aad7ca614283dea2
Alex Knott <alextknott@gmail.com>
2022-04-16 14:25:49 +0100

    Add contents to Homepage and type text with a typewriter effect

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Homepage = () => {
-  return <main aria-label="Home Section">Homepage</main>;
+  const typeEffect = () => {
+    let i = 0;
+    let text = "Welcome to my mock e-commerce website.";
+    let speed = 20;
+
+    const typeWriter = () => {
+      if (i < text.length) {
+        document.querySelector(".main-text").textContent += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    };
+    typeWriter();
+  };
+
+  useEffect(() => {
+    typeEffect();
+  }, []);
+
+  return (
+    <main aria-label="Home Section">
+      <h1>Homepage</h1>
+      <p className="main-text"></p>
+    </main>
+  );
 };
 
 export default Homepage;
