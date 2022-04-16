3bd6f2a7ff215a39e287d9a71a71f554a2b00d69
Alex Knott <alextknott@gmail.com>
2022-04-16 15:52:52 +0100

    Build out Catalogpage to return a CatalogItem for every element in itemArray

--- src/Components/Catalog/Catalogpage.jsx
+++ src/Components/Catalog/Catalogpage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import CatalogItem from "./CatalogItem";
+
 const Catalogpage = () => {
-  return <main aria-label="Catalog Section">Catalog</main>;
+  const itemArray = ["a", "b", "c", "d", "e"];
+  return (
+    <main aria-label="Catalog Section">
+      <h1>Catalog</h1>
+      <section className="items">
+        {itemArray.map((e, index) => (
+          <CatalogItem key={index} index={index} e={e} />
+        ))}
+      </section>
+    </main>
+  );
 };
 
 export default Catalogpage;
