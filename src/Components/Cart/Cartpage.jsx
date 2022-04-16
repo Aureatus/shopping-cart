e139231409ce08ed1553ed94954b2a98a5b881be
Alex Knott <alextknott@gmail.com>
2022-04-16 23:13:38 +0100

    Import Cartpage.css

--- src/Components/Cart/Cartpage.jsx
+++ src/Components/Cart/Cartpage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import CartItems from "./CartItems";
 
+import "../../Styles/Cart/Cartpage.css";
+
 const Cartpage = () => {
   const cartItemArray = ["a", "b", "c", "d", "e"];
 
