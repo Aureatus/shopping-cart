7f12fe6e9615b89aabf4a9d7c2a9b9b32c56430b
Alex Knott <alextknott@gmail.com>
2022-04-17 15:06:18 +0100

    Fetch products and pass to Catalogpage

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,25 @@ import Cartpage from "./Components/Cart/Cartpage";
 import Navbar from "./Components/Navbar/Navbar";
 
 import "./Styles/App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  let [shopData, setShopData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products?limit=12`, {
+      mode: "cors",
+    })
+      .then((response) => response.json())
+      .then((data) => setShopData(data));
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
       <Routes>
         <Route index element={<Homepage />} />
-        <Route path="catalog" element={<Catalogpage />} />
+        <Route path="catalog" element={<Catalogpage shopData={shopData} />} />
         <Route path="cart" element={<Cartpage />} />
       </Routes>
     </div>
