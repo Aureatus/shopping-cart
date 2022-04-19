c8cb4108c8042a1d072c93423c71633034422f02
Alex Knott <alextknott@gmail.com>
2022-04-19 14:15:45 +0100

    Render div without submit options if element has been added to cart already

--- src/Components/Catalog/Catalogpage.jsx
+++ src/Components/Catalog/Catalogpage.jsx
@@ -19,18 +19,34 @@ const Catalogpage = (props) => {
       <main aria-label="Catalog Section">
         <h1>Catalog</h1>
         <section className="items">
-          {shopData.map((e, index) => (
-            <CatalogItem
-              key={e.id}
-              index={index}
-              title={e.title}
-              image={e.image}
-              price={e.price}
-              id={e.id}
-              addToCart={props.addToCart}
-              changeItemAmount={props.changeItemAmount}
-            />
-          ))}
+          {shopData.map((e, index) => {
+            if (e.added === false) {
+              return (
+                <CatalogItem
+                  key={e.id}
+                  index={index}
+                  title={e.title}
+                  image={e.image}
+                  price={e.price}
+                  id={e.id}
+                  addToCart={props.addToCart}
+                  changeItemAmount={props.changeItemAmount}
+                />
+              );
+            } else if (e.added === true) {
+              return (
+                <div className="item" key={e.id}>
+                  <div className="itemImage">
+                    <img src={e.image} alt="" />
+                  </div>
+                  <p className="itemName">{e.title}</p>
+                  <h2 className="itemCost">£{e.price}</h2>
+                  <h1>Item In Cart.</h1>
+                </div>
+              );
+            }
+            return true;
+          })}
         </section>
       </main>
     );
