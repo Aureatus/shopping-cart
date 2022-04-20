88a7cd463d540ade145571de618135d200d202c9
Alex Knott <alextknott@gmail.com>
2022-04-20 12:53:52 +0100

    Put img tag in a div container

--- src/Components/Cart/CartItems.jsx
+++ src/Components/Cart/CartItems.jsx
@@ -5,7 +5,10 @@ import "../../Styles/Cart/CartItems.css";
 const CartItems = (props) => {
   return (
     <div className="cart-item">
-      <img src={props.image} alt="" />
+      <div className="cart-item-image">
+        <img src={props.image} alt="" />
+      </div>
+
       <p className="item-name">{props.title}</p>
       <p className="item-amount">Amount: {props.amount}</p>
       <p className="item-price">£{props.price}</p>
