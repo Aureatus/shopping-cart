451a55678c41a670805be0e5f881c2ba6f729daa
Alex Knott <alextknott@gmail.com>
2022-04-21 14:03:03 +0100

    Move 3 separate navbar tests into one suite

--- src/Components/Tests/Navbar/Navbar.test.jsx
+++ src/Components/Tests/Navbar/Navbar.test.jsx
@@ -7,24 +7,40 @@ import "@testing-library/jest-dom";
 import App from "../../../App";
 import userEvent from "@testing-library/user-event";
 
-test(`Full app navigation using Navbar Links`, async () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-  userEvent.click(screen.getByRole("link", { name: /catalog/i }));
-  expect(
-    screen.getByRole("main", { name: /catalog section/i })
-  ).toBeInTheDocument();
+describe("Full app navigation using Navbar Links", () => {
+  it("Loads catalog page upon clicking catalog link", async () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    userEvent.click(screen.getByRole("link", { name: /catalog/i }));
+    expect(
+      screen.getByRole("main", { name: /catalog section/i })
+    ).toBeInTheDocument();
+  });
 
-  userEvent.click(screen.getByRole("link", { name: /cart/i }));
-  expect(
-    screen.getByRole("main", { name: /cart section/i })
-  ).toBeInTheDocument();
+  it("Loads cart page upon clicking cart link", async () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    userEvent.click(screen.getByRole("link", { name: /cart/i }));
+    expect(
+      screen.getByRole("main", { name: /cart section/i })
+    ).toBeInTheDocument();
+  });
 
-  userEvent.click(screen.getByRole("link", { name: /home/i }));
-  expect(
-    screen.getByRole("main", { name: /home section/i })
-  ).toBeInTheDocument();
+  it("Loads home page upon clicking home link", async () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    userEvent.click(screen.getByRole("link", { name: /home/i }));
+    expect(
+      screen.getByRole("main", { name: /home section/i })
+    ).toBeInTheDocument();
+  });
 });
