9e59dff590afbab68d38d3f4028bbc61da65acaf
Alex Knott <alextknott@gmail.com>
2022-04-22 14:39:04 +0100

    Test that snapshot matches render

---
+++ src/Components/Tests/index.test.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import App from "../../App";
+
+it("Renders header and homepage correctly", () => {
+  const { container } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  expect(container).toMatchSnapshot();
+});
