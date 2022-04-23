3ed54aa1b1bb3d8a41d27f5f4a32806f7d3b646c
Alex Knott <alextknott@gmail.com>
2022-04-23 15:44:38 +0100

    Implement test to check if Homepage welcome text renders correctly

---
+++ src/Components/Tests/Homepage/Homepage.test.jsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+
+import { MemoryRouter } from "react-router-dom";
+import App from "../../../App";
+import "@testing-library/jest-dom";
+
+describe("Homepage renders correctly", () => {
+  it("Welcome text is typed out", async () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>
+    );
+
+    const welcomeText = await screen.findByText(
+      /welcome to my mock e\-commerce website\./i
+    );
+    expect(welcomeText).toBeInTheDocument();
+  });
+});
